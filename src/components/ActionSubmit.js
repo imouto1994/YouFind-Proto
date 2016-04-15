0047635a20ae4e398f0be97a6a2d13f51517ef77
Trong Nhan <imouto1994@gmail.com>
2016-04-15 14:36:56 +0800

    Add bulk message

--- src/components/ActionSubmit.js
+++ src/components/ActionSubmit.js
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router';
 import Cookies from 'js-cookie';
+import _ from 'lodash';
 
 import Utility from '../utils';
 import VideoCard from './VideoCard';
@@ -23,7 +24,12 @@ class ActionSubmit extends Component {
     this.state = {
       reportVideos,
       contactVideos,
-      requestVideos
+      requestVideos,
+      messageType: {
+        report: 'bulk',
+        contact: 'bulk',
+        request: 'bulk'
+      }
     };
   }
 
@@ -35,6 +41,14 @@ class ActionSubmit extends Component {
   onNext = () => {
     const { history } = this.props;
     history.push('/u/actionConfirm');
+  };
+
+  onRadioChange = (action, type) => {
+    const messageType = _.clone(this.state.messageType);
+    messageType[action] = type;
+    this.setState({
+      messageType
+    });
   };
 
   render() {
@@ -65,11 +79,41 @@ class ActionSubmit extends Component {
   }
 
   renderReport() {
-    const { reportVideos } = this.state;
+    const messageType = this.state.messageType.report;
 
     return (
       <div className={ `${CLASS_NAME}-report` }>
         <h3>Report Violation to Hosting Site</h3>
+        <div className="radio yf-margin-bottom-30">
+          <label>
+            <input type="radio" name="report"
+              checked={ messageType === 'bulk' }
+              onClick={ () => this.onRadioChange('report', 'bulk') } /> Bulk Message
+          </label>
+          <label className="yf-margin-left-15">
+            <input type="radio" name="report"
+              checked={ messageType === 'custom' }
+              onClick={ () => this.onRadioChange('report', 'custom') } /> Custom Message
+          </label>
+        </div>
+        {
+          messageType === 'bulk'
+          ? this.renderReportBulk()
+          : undefined
+        }
+        {
+          messageType === 'custom'
+          ? this.renderReportCustom()
+          : undefined
+        }
+      </div>
+    );
+  }
+
+  renderReportBulk() {
+    const { reportVideos } = this.state;
+    return (
+      <div>
         <h4>Selected Videos</h4>
         <div className="row">
           { reportVideos.map(this.renderVideo.bind(this)) }
@@ -84,17 +128,57 @@ class ActionSubmit extends Component {
     );
   }
 
+  renderReportCustom() {
+    const { reportVideos } = this.state;
+    return (
+      <div>
+        <h4>Selected Videos</h4>
+        { reportVideos.map(this.renderReportVideoWithMessage.bind(this)) }
+      </div>
+    );
+  }
+
   renderContact() {
-    const { contactVideos } = this.state;
+    const messageType = this.state.messageType.contact;
 
     return (
       <div className={ `${CLASS_NAME}-contact` }>
         <h3>Contact Video Owner</h3>
+        <div className="radio yf-margin-bottom-30">
+          <label>
+            <input type="radio" name="contact"
+              checked={ messageType === 'bulk' }
+              onClick={ () => this.onRadioChange('contact', 'bulk') } /> Bulk Message
+          </label>
+          <label className="yf-margin-left-15">
+            <input type="radio" name="contact"
+              checked={ messageType === 'custom' }
+              onClick={ () => this.onRadioChange('contact', 'custom') } /> Custom Message
+          </label>
+        </div>
+        {
+          messageType === 'bulk'
+          ? this.renderContactBulk()
+          : undefined
+        }
+        {
+          messageType === 'custom'
+          ? this.renderContactCustom()
+          : undefined
+        }
+      </div>
+    );
+  }
+
+  renderContactBulk() {
+    const { contactVideos } = this.state;
+    return (
+      <div>
         <h4>Selected Videos</h4>
         <div className="row">
           { contactVideos.map(this.renderVideo.bind(this)) }
         </div>
-        <h4>Message to Owner</h4>
+        <h4>Message to Site</h4>
         <div className="form-group">
           <textarea id="feedback-contact"
             className={ `${CLASS_NAME}-textarea form-control` }
@@ -104,12 +188,52 @@ class ActionSubmit extends Component {
     );
   }
 
+  renderContactCustom() {
+    const { contactVideos } = this.state;
+    return (
+      <div>
+        <h4>Selected Videos</h4>
+        { contactVideos.map(this.renderContactVideoWithMessage.bind(this)) }
+      </div>
+    );
+  }
+
   renderRequest() {
-    const { requestVideos } = this.state;
+    const messageType = this.state.messageType.request;
 
     return (
       <div className={ `${CLASS_NAME}-request` }>
         <h3>Request Linkback / Credit</h3>
+        <div className="radio yf-margin-bottom-30">
+          <label>
+            <input type="radio" name="request"
+              checked={ messageType === 'bulk' }
+              onClick={ () => this.onRadioChange('request', 'bulk') } /> Bulk Message
+          </label>
+          <label className="yf-margin-left-15">
+            <input type="radio" name="request"
+              checked={ messageType === 'custom' }
+              onClick={ () => this.onRadioChange('request', 'custom') } /> Custom Message
+          </label>
+        </div>
+        {
+          messageType === 'bulk'
+          ? this.renderRequestBulk()
+          : undefined
+        }
+        {
+          messageType === 'custom'
+          ? this.renderRequestCustom()
+          : undefined
+        }
+      </div>
+    );
+  }
+
+  renderRequestBulk() {
+    const { requestVideos } = this.state;
+    return (
+      <div>
         <h4>Selected Videos</h4>
         <div className="row">
           { requestVideos.map(this.renderVideo.bind(this)) }
@@ -133,10 +257,81 @@ class ActionSubmit extends Component {
     );
   }
 
+  renderRequestCustom() {
+    const { requestVideos } = this.state;
+    return (
+      <div>
+        <h4>Selected Videos</h4>
+        { requestVideos.map(this.renderRequestVideoWithMessage.bind(this)) }
+      </div>
+    );
+  }
+
   renderVideo(video, index) {
     return (
       <div className="col-xs-6 col-sm-4 col-md-3" key={ index }>
         <VideoCard video={ video } />
+      </div>
+    );
+  }
+
+  renderReportVideoWithMessage(video, index) {
+    return (
+      <div className="row yf-margin-bottom-30" key={ index }>
+        <div className="col-xs-12 col-sm-4">
+          <VideoCard video={ video } />
+        </div>
+        <div className="col-xs-12 col-sm-8">
+          <div className="form-group">
+            <textarea id={ `feedback-report-${index}` }
+              className={ `${CLASS_NAME}-textarea form-control` }
+              rows="8" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderContactVideoWithMessage(video, index) {
+    return (
+      <div className="row yf-margin-bottom-30" key={ index }>
+        <div className="col-xs-12 col-sm-4">
+          <VideoCard video={ video } />
+        </div>
+        <div className="col-xs-12 col-sm-8">
+          <div className="form-group">
+            <textarea id={ `feedback-contact-${index}` }
+              className={ `${CLASS_NAME}-textarea form-control` }
+              rows="8" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderRequestVideoWithMessage(video, index) {
+    return (
+      <div className="yf-margin-bottom-30" key={ index }>
+        <div className="row">
+          <div className="col-xs-12 col-sm-4">
+            <VideoCard video={ video } />
+          </div>
+          <div className="col-xs-12 col-sm-8">
+            <div className="form-group">
+              <textarea id={ `feedback-request-${index}` }
+                className={ `${CLASS_NAME}-textarea form-control` }
+                rows="8" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <input type="file"
+            ref="fileInput"
+            style={ { display: 'none' } } />
+          <div className="btn btn-primary" onClick={ this.onUploadButtonClick }>
+            Upload Watermarked Image
+          </div>
+        </div>
       </div>
     );
   }
